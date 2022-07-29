41b2f7160f608ebc4a700fd8da8c70fa52abdc9f
unknown <gilsoncdesigner@gmail.com>
2022-07-29 14:40:35 -0300

    Primeiro commit

---
+++ src/components/ShoppingList/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { FlatList } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
+import { styles } from './styles';
+import { Product, ProductProps } from '../Product';
+
+import { shoppingListExample } from '../../utils/shopping.list.data';
+
+export function ShoppingList() {
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    const subscribe = firestore()
+      .collection('products')
+      //.limit(3)
+      //.where('quantity', '==', 3)
+      //.orderBy('description', 'asc')
+      //.orderBy('quantity')
+      //.startAt(2)
+      //.endAt(4)
+      .onSnapshot(querySnapshot => {
+        const data = querySnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          }
+        }) as ProductProps[];
+
+        setProducts(data);
+      });
+
+    return () => subscribe();
+  }, []);
+
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => <Product data={item} />}
+      showsVerticalScrollIndicator={false}
+      style={styles.list}
+      contentContainerStyle={styles.content}
+    />
+  );
+}
